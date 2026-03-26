e1318f528eb98e5a404b91123fe7ab2b0f4c93ce
jittu0410 <jithenderkanna@gmail.com>
2026-03-26 19:56:26 +0530

    Handle exceptions in query processing

--- endpoints.py
+++ endpoints.py
@@ -9,6 +9,7 @@ async def query_rag_system(query: str):
         # Call the RAG system to process the query and get a response
         response = await get_rag_response(query)
         return {"query": query, "response": response}
+        
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
